c6a0ca18b7e8f11e388fef0e30de87dc14a631fc
rosdvp <45929573+rosdvp@users.noreply.github.com>
2024-05-02 20:10:19 +0300

    EditorForceReserializeMenuItem

---
+++ Editor/EditorForceReserializeMenuItem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace BlahEditor.Editor
+{
+public static class EditorForceReserializeMenuItem
+{
+	[MenuItem("Blah/Editor/Force Reserialize Assets")]
+	private static void ForceReserialize()
+	{
+		AssetDatabase.ForceReserializeAssets();
+	}
+}
+}
