f581ba7b89e62b8a231a9381bfedd6b99575daca
rosdvp <45929573+rosdvp@users.noreply.github.com>
2024-05-05 17:25:48 +0300

    [1.0.17] NullableField

---
+++ Nullables/NullableField.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using BlahEditor.DrawersExtensions;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace BlahEditor.Nullables
+{
+[Serializable]
+public struct NullableField<T>
+{
+	[SerializeField]
+	private bool _hasValue;
+	[SerializeField]
+	private T _value;
+
+	public T Value
+	{
+		get => _hasValue ? _value : throw new Exception("_hasValue is false");
+		set => _value = value;
+	}
+
+	public bool HasValue => _hasValue;
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(NullableField<>))]
+public class EditorNullableFloatDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect rect, SerializedProperty prop, GUIContent label)
+	{
+		var propHasValue = prop.FindPropertyRelative("_hasValue");
+		var propValue    = prop.FindPropertyRelative("_value");
+
+		var rects = rect.SplitHorizontal(5, 0.4f, 0.05f, 0.55f);
+
+		EditorGUI.LabelField(rects[0], label);
+		EditorGUI.PropertyField(rects[1], propHasValue, GUIContent.none);
+
+		if (propHasValue.boolValue)
+			EditorGUI.PropertyField(rects[2], propValue, GUIContent.none);
+	}
+}
+#endif
+}
